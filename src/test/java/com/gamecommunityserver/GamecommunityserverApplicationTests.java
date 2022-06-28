a2424b7222ba7c71639c0c161cb52e1c3a996d83
gamsayeon <aud4551@naver.com>
2022-06-28 21:41:33 +0900

    inialized commit

---
+++ src/test/java/com/gamecommunityserver/GamecommunityserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamecommunityserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamecommunityserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
