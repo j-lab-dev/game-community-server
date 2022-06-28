2f46493ff9ebd4c7989898fe00301b41abe08eeb
gamsayeon <aud4551@naver.com>
2022-06-28 22:26:21 +0900

    ENH: 코드리뷰를 위한 브랜치 머지 테스트 입니다.

--- src/main/java/com/gamecommunityserver/GamecommunityserverApplication.java
+++ src/main/java/com/gamecommunityserver/GamecommunityserverApplication.java
@@ -3,6 +3,12 @@ package com.gamecommunityserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * TODO: @SpringBootApplication 동작방식 톰캣을 구하는 순서 서치해오.
+ * 내장톰캣 vs 외장톰캣 차이서치해오기.
+ *
+ */
 @SpringBootApplication
 public class GamecommunityserverApplication {
 
---
+++ src/main/java/com/gamecommunityserver/service/UserService.java
@@ -0,0 +1,7 @@
+package com.gamecommunityserver.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
